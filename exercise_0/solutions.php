58163a5a3b9391ccc40f3f1586fdd0b071f6028d
edgarsj <edgaars@gmail.com>
2020-04-23 22:04:29 +0300

    first commit

---
+++ exercise_0/solutions.php
@@ -0,0 +1,52 @@
+<?php
+
+//+++++++++++++++++++++++++Problem 1+++++++++++++++++++++++++++++++++++++
+function findOddEvenPair(array $numbers): int
+{
+    for ($i = 0; $i < count($numbers); $i++) {
+        if ($i < count($numbers) - 1) {
+            if (($numbers[$i] % 2 === 0 && $numbers[$i + 1] % 2 !== 0) ||
+                ($numbers[$i] % 2 !== 0 && $numbers[$i + 1] % 2 === 0)) {
+                return $i;
+            }
+        }
+    }
+}
+
+//echo(findOddEvenPair($nums = [1, 2, 4, 3, 7]));
+
+
+//++++++++++++++PROBLEM 2+++++++++++++++++++++
+class SummationService
+{
+    protected array $data;
+
+    public function __construct(array $data)
+    {
+        $this->data = $data;
+    }
+
+    public function sum(int $a, int $b): int
+    {
+        $sum = 0;
+        for ($i = $a; $i <= $b; $i++) {
+            $sum += $this->data[$i];
+        }
+        return $sum;
+    }
+}
+
+//$service = new SummationService([-1, 0, 2, 7, -15]);
+//echo($service->sum(1,3));
+
+
+
+//+++++++++++++++++++++++++PROBLEM 3++++++++++++++++++++++++++++++++
+function longestSubstr(string $text): string
+{
+    throw new \Exception('Not implemented.');
+}
+
+
+
+
---
+++ exercise_1/solutions.php
@@ -0,0 +1,124 @@
+<?php
+//----------------------------------------------------
+//------------------PROBLEM 1-------------------------
+//----------------------------------------------------
+
+function maxProfit(array $pricesAndPurchases): int
+{
+    //Making array with all the prices and corresponding days.
+    $pricesAndDays = [];
+    foreach ($pricesAndPurchases as $day => $value) {
+        $pricesAndDays[] = ['price' => $value['price'], 'day' => $day];
+    }
+
+    //Sorting pricesAndDays array by price value in descending order.
+    function mySort($a, $b)
+    {
+        if ($a['price'] === $b['price']) {
+            return 1;
+        }
+        return ($a['price'] > $b['price']) ? -1 : 1;
+    }
+
+    usort($pricesAndDays, 'mySort');
+
+    $daysToSell = [];
+    //Adding highest-price day as a starting point to daysToSell.
+    $daysToSell[] = $pricesAndDays[0]['day'];
+
+    /*
+       Adding to daysToSell array days, that ar higher than highest-price day
+       from sorted priceAndDays array.
+    */
+    for ($i = 1; $i < count($pricesAndDays); $i++) {
+        if ($pricesAndDays[$i]['day'] > $daysToSell[count($daysToSell) - 1]) {
+            $daysToSell[] = $pricesAndDays[$i]['day'];
+        }
+    }
+
+    $currentStock = 0;
+    $totalMoneySpent = 0;
+    $totalRevenue = 0;
+
+    foreach ($pricesAndPurchases as $day => $value) {
+        $totalMoneySpent += $value['price'] * $value['purchased'];
+        $currentStock += $value['purchased'];
+
+        //If daysToSell array contains current day. We sell everything.
+        if (in_array($day, $daysToSell)) {
+            $totalRevenue += $value['price'] * $currentStock;
+            $currentStock = 0;
+        }
+    }
+    return $totalRevenue - $totalMoneySpent;
+
+}
+
+//$oilMarket = [
+//    0 => ['price' => 2, 'purchased' => 1],
+//    1 => ['price' => 8, 'purchased' => 1],
+//    2 => ['price' => 10, 'purchased' => 1],
+//    3 => ['price' => 4, 'purchased' => 1],
+//    4 => ['price' => 9, 'purchased' => 1]
+//
+//];
+//echo(maxProfit($oilMarket));
+
+
+//----------------------------------------------------
+//------------------PROBLEM 2-------------------------
+//----------------------------------------------------
+
+function stringCost(string $a, string $b,
+                    int $insertionCost, int $deletionCost, int $replacementCost):int
+{
+    //Checking if replacing is more expensive than delete+insert
+    $isReplaceMoreExpensive = false;
+    if (($insertionCost + $deletionCost) < $replacementCost) {
+        $isReplaceMoreExpensive = true;
+    }
+    $first = str_split($a);
+    $targetWord = str_split($b);
+    $result = [];
+    $moneySpent = 0;
+
+    if (strlen($a) <= strlen($b)) {
+        $howManyToInsert = strlen($b) - strlen($a);
+        for ($i = 0; $i < count($first); $i++) {
+            if ($first[$i] !== $targetWord[$i]) {
+                if ($isReplaceMoreExpensive) {
+                    $moneySpent += ($deletionCost + $insertionCost);
+                } else {
+                    $moneySpent += $replacementCost;
+                }
+            }
+            $result[] = $targetWord[$i];
+        }
+        $moneySpent += $howManyToInsert * $insertionCost;
+    } else {
+        $howManyDeletes = strlen($a) - strlen($b);
+        for ($i = 0; $i < count($targetWord); $i++) {
+            if ($first[$i] !== $targetWord[$i]) {
+                if ($isReplaceMoreExpensive) {
+                    $moneySpent += ($deletionCost + $insertionCost);
+                } else {
+                    $moneySpent += $replacementCost;
+                }
+            }
+            $result[] = $targetWord[$i];
+        }
+        $moneySpent += $howManyDeletes * $deletionCost;
+    }
+    return $moneySpent;
+
+
+}
+
+//echo(stringCost('bitten', 'meeting', 2, 3, 6));
+
+
+//------------------PROBLEM 3-------------------------
+function incrementalMedian(array $values): array
+{
+    throw new \Exception('Not implemented.');
+}
