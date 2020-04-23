62e5ead4477e7e2fdc6f5a8e15f7bbe0d7236cce
edgarsj <edgaars@gmail.com>
2020-04-23 23:37:52 +0300

    finished problem 3 @ exercise_0

--- exercise_0/solutions.php
+++ exercise_0/solutions.php
@@ -40,12 +40,36 @@ class SummationService
 //echo($service->sum(1,3));
 
 
-
 //+++++++++++++++++++++++++PROBLEM 3++++++++++++++++++++++++++++++++
 function longestSubstr(string $text): string
 {
-    throw new \Exception('Not implemented.');
+    $pairs = [];
+
+    $helper = 2;
+    for ($i = 0; $i < strlen($text) - 1; $i++) {
+        $pairs[] = substr($text, $i, $helper);
+    }
+
+    $testStr = '';
+    $result = '';
+    $longestReached = false;
+    for ($i = 0; $i < strlen($text); $i++) {
+        $testStr .= $text[$i];
+        foreach ($pairs as $pair) {
+            if (substr_count($testStr, $pair) > 1) {
+                $longestReached = true;
+            };
+
+        }
+        if ($longestReached) {
+            break;
+        }
+        $result .= $text[$i];
+    }
+    return $result;
 }
+
+//echo(longestSubstr('aZAzaz'));
 
 
 
